ec9c64f8daa26e5fea3955ebecdb9be2aa486f0a
oleg <mattuhin.oleg@gmail.com>
2020-11-23 20:17:53 +0300

    test2

--- TestConversion/ViewController.swift
+++ TestConversion/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
             
-        
+        print("bug not fixed yet")
         let scnView = SCNView()
         
         scnView.scene = SCNScene()
